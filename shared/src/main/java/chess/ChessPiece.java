2ce1f0411452b08a2be957570039bfcf66213dbc
Logan Gibbons <lgg25@byu.edu>
2025-01-20 16:43:21 -0700

    Finish logic for Bishops

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -92,6 +92,66 @@ public class ChessPiece {
         ArrayList<ChessMove> moves = new ArrayList<>();
 
         // Going Up and Right
+        for (int i = 1; i < 9; i ++) {
+            ChessPosition upAndRight = new ChessPosition(startPosition.getRow() + i, startPosition.getColumn() + i);
+
+            if (!inbounds(upAndRight) || (board.getPiece(upAndRight) != null && getTeamColor() == board.getPiece(upAndRight).getTeamColor())){
+                break;
+            }
+
+            moves.add(new ChessMove(startPosition, upAndRight, null));
+
+            if (board.getPiece(upAndRight) != null && getTeamColor() != board.getPiece(upAndRight).getTeamColor()){
+                break;
+            }
+        }
+
+        // Going Up and Left
+        for (int i = 1; i < 9; i ++) {
+            ChessPosition upAndLeft = new ChessPosition(startPosition.getRow() + i, startPosition.getColumn() - i);
+
+            if (!inbounds(upAndLeft) || (board.getPiece(upAndLeft) != null && getTeamColor() == board.getPiece(upAndLeft).getTeamColor())){
+                break;
+            }
+
+            moves.add(new ChessMove(startPosition, upAndLeft, null));
+
+            if (board.getPiece(upAndLeft) != null && getTeamColor() != board.getPiece(upAndLeft).getTeamColor()){
+                break;
+            }
+        }
+
+
+        //Going Down and right
+        for (int i = 1; i < 9; i ++) {
+            ChessPosition downAndRight = new ChessPosition(startPosition.getRow() - i, startPosition.getColumn() + i);
+
+            if (!inbounds(downAndRight) || (board.getPiece(downAndRight) != null && getTeamColor() == board.getPiece(downAndRight).getTeamColor())){
+                break;
+            }
+
+            moves.add(new ChessMove(startPosition, downAndRight, null));
+
+            if (board.getPiece(downAndRight) != null && getTeamColor() != board.getPiece(downAndRight).getTeamColor()){
+                break;
+            }
+        }
+
+
+        // Going Down and Left
+        for (int i = 1; i < 9; i ++) {
+            ChessPosition downAndLeft = new ChessPosition(startPosition.getRow() - i, startPosition.getColumn() - i);
+
+            if (!inbounds(downAndLeft) || (board.getPiece(downAndLeft) != null && getTeamColor() == board.getPiece(downAndLeft).getTeamColor())){
+                break;
+            }
+
+            moves.add(new ChessMove(startPosition,(downAndLeft), null));
+
+            if (board.getPiece(downAndLeft) != null && getTeamColor() != board.getPiece(downAndLeft).getTeamColor()){
+                break;
+            }
+        }
 
 
         return moves;
