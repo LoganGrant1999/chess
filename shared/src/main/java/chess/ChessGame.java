c8da69b2cc23a1748cc321c6c6078f32b2b7dc4c
Logan Gibbons <lgg25@byu.edu>
2025-01-27 15:14:29 -0700

    create setBoard and getBoard methods

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,9 +2,6 @@ package chess;
 
 import java.util.Collection;
 
-import static chess.ChessGame.TeamColor.BLACK;
-import static chess.ChessGame.TeamColor.WHITE;
-
 /**
  * For a class that can manage a chess game, making moves on a board
  * <p>
@@ -105,7 +102,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board = board;
     }
 
     /**
@@ -114,7 +111,7 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return board;
     }
 
 }
