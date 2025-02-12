635a5f48c5874071e2c8db29eb7facb488ca7e67
Logan Gibbons <lgg25@byu.edu>
2025-02-12 14:13:29 -0700

    Create DAO and Model classes

--- server/src/main/java/handlers/RegisterHandler.java
+++ server/src/main/java/handlers/RegisterHandler.java
@@ -1,11 +1,14 @@
 package handlers;
 
+import request.RegisterRequest;
 import spark.Request;
 import spark.Response;
 
 public class RegisterHandler extends BaseHandler {
     @Override
     public Object handle(Request request, Response response) throws Exception {
+
+        RegisterRequest req = gson.fromJson(request.body(), RegisterRequest.class);
         return null;
     }
 }
