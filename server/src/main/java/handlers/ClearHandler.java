f59b73bf84a0c1b627e6e028d5b9d6898ac947a0
Logan Gibbons <lgg25@byu.edu>
2025-02-19 17:01:52 -0700

    Make ClearResponse and ClearHandler

--- server/src/main/java/handlers/ClearHandler.java
+++ server/src/main/java/handlers/ClearHandler.java
@@ -1,15 +1,45 @@
 package handlers;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import response.ClearResponse;
+import service.ClearService;
 import spark.Request;
 import spark.Response;
 
 public class ClearHandler extends BaseHandler{
 
+    private MemoryAuthDAO auth;
+    private MemoryGameDAO game;
+    private MemoryUserDAO user;
 
 
+    public ClearHandler(MemoryAuthDAO auth, MemoryGameDAO game, MemoryUserDAO user) {
+        this.auth = auth;
+        this.game = game;
+        this.user = user;
+    }
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        return null;
+
+        try {
+
+            ClearService clearService = new ClearService();
+
+            clearService.clearer(auth, game, user);
+
+            response.status(200);
+
+            return new ClearResponse();
+
+        } catch (DataAccessException e){
+
+            response.status(500);
+
+            return new ErrorFormatter(e).getErrorFormat();
+        }
     }
 }
---
+++ server/src/main/java/service/ClearService.java
@@ -0,0 +1,22 @@
+package service;
+
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import response.ClearResponse;
+
+public class ClearService {
+
+
+    public ClearResponse clearer(MemoryAuthDAO auth, MemoryGameDAO game, MemoryUserDAO user) throws Exception{
+
+        return null;
+
+    }
+
+
+
+}
