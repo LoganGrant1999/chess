f810c066af9201b89064094045cbda8ce01adacd
Logan Gibbons <lgg25@byu.edu>
2025-03-08 14:14:26 -0700

    Wrote insertUser method and created DataAccessTests

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -2,15 +2,50 @@ package dataaccess;
 
 import model.UserData;
 
+import javax.xml.crypto.Data;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import static java.lang.foreign.MemorySegment.NULL;
 
 //Placeholder class for Phase4 when I'll interact with the database and store UserData there
-public class MySqlUserDAO implements UserDAO{
+public class MySqlUserDAO implements UserDAO {
 
+
+    private int executeUpdate(String statement, Object... params) throws Exception{
+        try(var conn = DatabaseManager.getConnection()){
+            try(var prepStatement = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)){
+                for (var i = 0; i < params.length; i++){
+                    var param = params[i];
+                    if (param instanceof String p) prepStatement.setString(i + 1, p);
+                }
+                prepStatement.executeUpdate();
+
+                var keys = prepStatement.getGeneratedKeys();
+                if (keys.next()){
+                    return keys.getInt(1);
+                }
+
+                return 0;
+            }
+
+        } catch (SQLException e){
+
+            throw new DataAccessException(e.getMessage());
+        }
+    }
 
     @Override
     public void createUser(UserData userData) throws DataAccessException {
 
+        try {
+            var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+            executeUpdate(statement, userData.username(), userData.password(), userData.email());
+
+        } catch (Exception e) {
+
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
