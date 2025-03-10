cba2bc37e96404eb75efbf24965416103dd16e12
Logan Gibbons <lgg25@byu.edu>
2025-03-10 15:22:27 -0600

    Set Before and After Each's for unit tests and wrote unit test for successful createUser

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -20,6 +20,7 @@ public class Server {
     }
 
     public int run(int desiredPort) {
+
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,11 +1,88 @@
 package dataaccess;
 
-import org.junit.jupiter.api.Test;
+import handlers.ClearHandler;
+import model.UserData;
+import org.junit.jupiter.api.*;
+import org.mindrot.jbcrypt.BCrypt;
+import server.Server;
+import service.ClearService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class DataAccessTests {
-    @Test
-    void createUser() {
+
+    private static MySqlAuthDAO auth;
+    private static MySqlGameDAO game;
+    private static MySqlUserDAO user;
+    private static Server server;
+    private static ClearService clearer;
+
+    @BeforeEach
+    public void setup() throws Exception {
+
+        auth = new MySqlAuthDAO();
+
+        game = new MySqlGameDAO();
+
+        user = new MySqlUserDAO();
+
+        server = new Server();
+
+        server.run(0);
+
+        DatabaseManager.configureDatabase();
+
+        clearer = new ClearService();
+
+        clearer.clearAllData(auth, game, user);
+
+        DatabaseManager.configureDatabase();
+
     }
+
+
+    @AfterEach
+    public void stopServer(){
+
+        server.stop();
+    }
+
+
+    @Test
+    @Order(1)
+    @DisplayName("Successful Create User Test")
+    void successfulCreateUser() throws DataAccessException {
+
+        UserData userData = new UserData("username", "password", "email@email.com");
+
+        user.createUser(userData);
+
+        server.stop();
+
+        server.run(0);
+
+        UserData storedUserData = user.getUser(userData.username());
+
+        assertEquals(userData.username(), storedUserData.username(), "Username not stored correctly");
+
+        assertEquals(userData.email(), storedUserData.email(), "Email not stored correctly");
+
+        assertTrue(BCrypt.checkpw(userData.password(), storedUserData.password()));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Test
     void getUser() {
