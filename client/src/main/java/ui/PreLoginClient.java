a1810719f2cb34ef418f1f949b609516e6fa132e
Logan Gibbons <lgg25@byu.edu>
2025-03-21 14:49:30 -0600

    Make it so that client main creates a Repl object and implement help, quit, and login in PreLoginClient as well as the eval method

--- client/src/main/java/ui/PreLoginClient.java
+++ client/src/main/java/ui/PreLoginClient.java
@@ -1,8 +1,15 @@
 package ui;
 
+import exceptions.NetworkException;
+import request.LoginRequest;
+import response.LoginResponse;
 import server.ServerFacade;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 public class PreLoginClient {
+
 
     private final ServerFacade server;
 
@@ -14,6 +21,56 @@ public class PreLoginClient {
 
         this.serverUrl = serverUrl;
     }
+
+    public String eval(String input){
+
+        try {
+
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "help" -> help();
+                case "quit" -> "quit";
+                case "login" -> login(params);
+                case "register" -> register(params);
+                default -> "Command unknown. Type 'help' to see all valid commands";
+            };
+        } catch (NetworkException e) {
+
+            return e.getMessage();
+        }
+    }
+
+    public String help() {
+
+        return """
+                - Help
+                - Quit
+                - Login <username> <password>
+                - Register <username> <password> <email>
+                """;
+    }
+
+    public String login(String... params) throws NetworkException {
+
+        if (params.length == 2) {
+
+            LoginRequest req = new LoginRequest(params[0], params[1]);
+
+            LoginResponse resp = server.login(req);
+
+            return String.format("You signed in as %s!", resp.username());
+        }
+
+        throw new NetworkException(400, "Expected: <username> <password>");
+    }
+
+    public String register(String... params) {
+
+        return null;
+    }
+
 
 
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,7 +1,6 @@
 package ui;
 
 import java.util.Scanner;
-import ui.EscapeSequences.*;
 public class Repl {
 
     private final PreLoginClient prelogin;
@@ -11,9 +10,43 @@ public class Repl {
         prelogin = new PreLoginClient(serverUrl);
     }
 
+    public void run() {
+
+        System.out.println(EscapeSequences.WHITE_KNIGHT + "Welcome to Chess. Sign in to start.");
+
+        System.out.print(prelogin.help());
+
+        Scanner scanner = new Scanner(System.in);
+
+        var result = "";
+
+        while (!result.equals("quit")) {
+
+            printPrompt();
+
+            String line = scanner.nextLine();
+
+            try {
+
+                result = prelogin.eval(line);
+
+                System.out.print(EscapeSequences.SET_TEXT_COLOR_RED + result);
+
+            } catch (Throwable e)  {
+
+                var msg = e.toString();
+
+                System.out.println(msg);
+
+            }
+
+            System.out.println();
+        }
+    }
 
 
+    private void printPrompt() {
 
-
-
+        System.out.print("\n" + EscapeSequences.RESET_TEXT_COLOR+ ">>> " + EscapeSequences.SET_TEXT_COLOR_MAGENTA);
+    }
 }
