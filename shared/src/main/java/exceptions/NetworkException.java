06b2af0a03c16294122c6cb8144c5162196eada2
Logan Gibbons <lgg25@byu.edu>
2025-03-20 18:28:59 -0600

    Add writeBody, throwIfNotSuccessful, readbody, and makeRequest methods to ServerFacade and Create NetworkException class ServerFacade

---
+++ shared/src/main/java/exceptions/NetworkException.java
@@ -0,0 +1,32 @@
+package exceptions;
+
+import com.google.gson.Gson;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NetworkException extends Exception {
+    final private int statusCode;
+
+    public NetworkException(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(Map.of("message", getMessage(), "status", statusCode));
+    }
+
+    public static NetworkException fromJson(InputStream stream) {
+        var map = new Gson().fromJson(new InputStreamReader(stream), HashMap.class);
+        var status = ((Double)map.get("status")).intValue();
+        String message = map.get("message").toString();
+        return new NetworkException(status, message);
+    }
+
+    public int StatusCode() {
+        return statusCode;
+    }
+}
--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,6 +1,7 @@
 package server;
 
 import com.google.gson.Gson;
+import exceptions.NetworkException;
 
 import java.io.*;
 import java.net.*;
@@ -14,11 +15,63 @@ public class ServerFacade {
     }
 
 
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws NetworkException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
 
-
-
-
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (NetworkException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new NetworkException(500, ex.getMessage());
+        }
     }
 
 
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, NetworkException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw NetworkException.fromJson(respErr);
+                }
+            }
+            throw new NetworkException(status, "other failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+
 }
+
