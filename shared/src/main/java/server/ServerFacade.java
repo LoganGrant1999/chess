f7adb6657e0d9617050b05d56ec83b8bf584996a
Logan Gibbons <lgg25@byu.edu>
2025-03-20 18:08:25 -0600

    Add constructor for ServerFacade

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,5 +1,24 @@
 package server;
 
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.*;
+
 public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+
+
+
+
+
+    }
+
+
 }
-.
