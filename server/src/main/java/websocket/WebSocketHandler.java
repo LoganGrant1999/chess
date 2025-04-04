25f74098f28ed676e071bc29d5df83d58a7c6899
Logan Gibbons <lgg25@byu.edu>
2025-04-04 13:11:47 -0600

    Wrote WebSocketHandler onMessage method

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -1,8 +1,10 @@
 package websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
 
 @WebSocket
 public class WebSocketHandler {
@@ -12,9 +14,32 @@ public class WebSocketHandler {
     @OnWebSocketMessage
     public void onMessage(Session session, String message){
 
+        UserGameCommand cmd = new Gson().fromJson(message, UserGameCommand.class);
 
+        switch (cmd.getCommandType()) {
+
+            case CONNECT -> connect();
+            case MAKE_MOVE -> makeMove();
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+        }
     }
 
 
+    public void connect() {
+
+    }
+
+    public void makeMove() {
+
+    }
+
+    public void leave() {
+
+    }
+
+    public void resign() {
+
+    }
 
 }
