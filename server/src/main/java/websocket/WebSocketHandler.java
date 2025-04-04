0e89fe1ad60972710a1306843e716f5fe23006fa
Logan Gibbons <lgg25@byu.edu>
2025-04-04 12:52:28 -0600

    Start WebSocketHandler class

---
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -0,0 +1,20 @@
+package websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
+public class WebSocketHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message){
+
+
+    }
+
+
+
+}
