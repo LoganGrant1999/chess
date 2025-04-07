fffc46373463e465cc4ad0555680ae93c2bdea09
Logan Gibbons <lgg25@byu.edu>
2025-04-06 19:49:14 -0600

    Set up WebSocketServerFacade connection

--- client/src/main/java/server/WebSocketServerFacade.java
+++ client/src/main/java/server/WebSocketServerFacade.java
@@ -1,18 +1,61 @@
 package server;
 
-import javax.websocket.Endpoint;
-import javax.websocket.EndpointConfig;
-import javax.websocket.Session;
+import com.google.gson.Gson;
+import exceptions.NetworkException;
+import websocket.messages.ServerMessage;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class WebSocketServerFacade extends Endpoint {
 
 
     Session session;
+    NotificationHandler notificationHandler;
 
-    public WebSocketServerFacade(String url)
+    public WebSocketServerFacade(String url, NotificationHandler notificationHandler) throws NetworkException {
+
+        try{
+
+            url = url.replace("http", "ws");
+
+            URI socketURI = new URI(url + "/ws");
+
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>(){
+
+                @Override
+                public void onMessage(String s) {
+
+                    ServerMessage msg = new Gson().fromJson(s, ServerMessage.class);
+
+                    notificationHandler.notify(msg);
+
+                }
+            });
+
+        } catch (URISyntaxException | DeploymentException | IOException e){
+
+            throw new NetworkException(500, e.getMessage());
+        }
+    }
 
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
 
     }
+
+
+
+
+
+
+
 }
