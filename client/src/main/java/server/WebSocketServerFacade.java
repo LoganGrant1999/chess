37f25d1eab06ccba8e022a6ea7c73bfc602f13be
Logan Gibbons <lgg25@byu.edu>
2025-04-06 19:34:05 -0600

    Create NotificationHandler and ws serverfacade

---
+++ client/src/main/java/server/WebSocketServerFacade.java
@@ -0,0 +1,18 @@
+package server;
+
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.Session;
+
+public class WebSocketServerFacade extends Endpoint {
+
+
+    Session session;
+
+    public WebSocketServerFacade(String url)
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+
+    }
+}
